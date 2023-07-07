e8022da21758674dc168fa94821013fd57a1cc62
Dharini-MernStack <dharinisrinivasan.94@gmail.com>
2023-07-07 19:50:58 +0530

    MAilto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -151,7 +151,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#C71610]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="dharinisrinivasan.94@gmail.com"
+              href="mailto:dharinisrinivasan.94@gmail.com"
             >
               Email
               <HiOutlineMail size={30} />
